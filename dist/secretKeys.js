b9562f27847df7e37c33595b96bce2dfbbf4f909
Valentine Bassey <54159265+awasevalentine@users.noreply.github.com>
2020-09-24 15:27:48 +0100

    Delete secretKeys.js

--- dist/secretKeys.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGODB_URL = void 0;
-exports.MONGODB_URL = "mongodb+srv://Valentine:bassey1992@hotelmanagement.vwtb0.gcp.mongodb.net/todoDb?retryWrites=true&w=majority";
-//# sourceMappingURL=secretKeys.js.map
